c807581ec47490bf34d115a37906b86f62b65635
miurakaikaito <56814146+miurakaikaito@users.noreply.github.com>
2019-10-24 10:39:17 +0900

    変更しました

--- app/controllers/hogehoges_controller.rb
+++
@@ -1,2 +0,0 @@
-class HogehogesController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  # get 'rails/d'
-
-  # get 'rails/controller'
-
-  # get 'rails/articles'
-
   root to:  'messages#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
